c7995c490a6fbeda1180340efbd48d6c6ce33eec
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-05-01 15:17:34 +0100

    initial commit

---
+++ src/main/java/com/yvolabs/rest/api/mongodb/RestApiMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.rest.api.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiMongodbApplication.class, args);
+    }
+
+}
